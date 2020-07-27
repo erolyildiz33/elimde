d9c9cc4eeebe2cdeafee4b4d784497d380f1e30f
Erol YILDIZ <erol_yildiz@msn.com>
2020-07-27 08:46:50 +0300

    ilk commit

---
+++ application/logs/log-2020-07-24.php
@@ -0,0 +1,23 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2020-07-24 09:39:19 --> Severity: Notice --> Trying to access array offset on value of type null C:\Users\Erol\Desktop\elimde\application\views\website\header.php 816
+ERROR - 2020-07-24 09:39:19 --> Severity: Notice --> Trying to access array offset on value of type null C:\Users\Erol\Desktop\elimde\application\views\website\header.php 824
+ERROR - 2020-07-24 09:39:19 --> Severity: Notice --> Trying to access array offset on value of type null C:\Users\Erol\Desktop\elimde\application\views\website\header.php 859
+ERROR - 2020-07-24 09:39:20 --> 404 Page Not Found: Img/slides
+ERROR - 2020-07-24 09:39:21 --> Severity: Notice --> Trying to access array offset on value of type null C:\Users\Erol\Desktop\elimde\application\views\website\header.php 816
+ERROR - 2020-07-24 09:39:21 --> Severity: Notice --> Trying to access array offset on value of type null C:\Users\Erol\Desktop\elimde\application\views\website\header.php 824
+ERROR - 2020-07-24 09:39:21 --> Severity: Notice --> Trying to access array offset on value of type null C:\Users\Erol\Desktop\elimde\application\views\website\header.php 859
+ERROR - 2020-07-24 09:39:23 --> 404 Page Not Found: Img/slides
+ERROR - 2020-07-24 10:52:15 --> Severity: Notice --> Trying to access array offset on value of type null C:\Users\Erol\Desktop\elimde\application\views\website\header.php 816
+ERROR - 2020-07-24 10:52:15 --> Severity: Notice --> Trying to access array offset on value of type null C:\Users\Erol\Desktop\elimde\application\views\website\header.php 824
+ERROR - 2020-07-24 10:52:15 --> Severity: Notice --> Trying to access array offset on value of type null C:\Users\Erol\Desktop\elimde\application\views\website\header.php 859
+ERROR - 2020-07-24 10:52:19 --> 404 Page Not Found: Img/slides
+ERROR - 2020-07-24 10:52:20 --> 404 Page Not Found: Assets/website
+ERROR - 2020-07-24 10:52:20 --> Severity: Notice --> Trying to access array offset on value of type null C:\Users\Erol\Desktop\elimde\application\views\website\header.php 816
+ERROR - 2020-07-24 10:52:20 --> Severity: Notice --> Trying to access array offset on value of type null C:\Users\Erol\Desktop\elimde\application\views\website\header.php 824
+ERROR - 2020-07-24 10:52:20 --> Severity: Notice --> Trying to access array offset on value of type null C:\Users\Erol\Desktop\elimde\application\views\website\header.php 859
+ERROR - 2020-07-24 10:52:22 --> Severity: Notice --> Trying to access array offset on value of type null C:\Users\Erol\Desktop\elimde\application\views\website\header.php 816
+ERROR - 2020-07-24 10:52:22 --> Severity: Notice --> Trying to access array offset on value of type null C:\Users\Erol\Desktop\elimde\application\views\website\header.php 824
+ERROR - 2020-07-24 10:52:22 --> Severity: Notice --> Trying to access array offset on value of type null C:\Users\Erol\Desktop\elimde\application\views\website\header.php 859
+ERROR - 2020-07-24 10:52:23 --> 404 Page Not Found: Assets/website
+ERROR - 2020-07-24 10:52:27 --> 404 Page Not Found: Img/slides
